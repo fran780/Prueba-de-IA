3293e027c0f44d2d0a83242d68b42e8bfef3799c
fran780 <franciscofernandez365@yahoo.com>
2025-03-17 22:24:56 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    let usuario = document.getElementById("usuario");
+    let contraseña = document.getElementById("contraseña");
+    let errorUsuario = document.getElementById("error-usuario");
+    let errorContraseña = document.getElementById("error-contraseña");
+
+    let valid = true;
+
+    if (usuario.value.trim() === "") {
+        errorUsuario.style.display = "block";
+        valid = false;
+    } else {
+        errorUsuario.style.display = "none";
+    }
+
+    if (contraseña.value.trim() === "") {
+        errorContraseña.style.display = "block";
+        valid = false;
+    } else {
+        errorContraseña.style.display = "none";
+    }
+
+
+    const usuarioCorrecto = "admin";
+    const contraseñaCorrecta = "12345";
+
+    if (valid) {
+        if (usuario.value.trim() === usuarioCorrecto && contraseña.value.trim() === contraseñaCorrecta) {
+            window.location.href = "menu.html";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    }
+});
